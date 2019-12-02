b4cf2b3af4949aa82906d86d0b5db70adccaf239
MrWang <1350989027@qq.com>
2019-12-02 10:41:49 +0800

    添加求二叉树最大深度于最小深度的函数

--- binary_tree/bin_tree.c
+++ binary_tree/bin_tree.c
@@ -5,6 +5,7 @@
 */
 void func(void)
 {
+    int maxdepth,mindepth;
     printf("func...\n");
     BiTNode *Troot;
     Troot = (BiTNode*)malloc(sizeof(BiTNode));
@@ -15,6 +16,10 @@ void func(void)
     printf("\n");
     InOrderTraverse(Troot);
     printf("\n");
+    maxdepth = maxDepth(Troot);
+    printf("the max depth of this tree is %d\n",maxdepth);
+    mindepth = minDepth(Troot);
+    printf("the min depth of this tree is %d\n",mindepth);
 }
 
 
@@ -58,7 +63,46 @@ void InOrderTraverse(BiTree T)
 }
 
 
-//找到二叉树的大深度
+/*
+Leetcode 104
+找到二叉树的大深度
+*/
+int maxDepth(BiTree T)
+{
+    if(T==NULL)
+        return 0;
+    int lDepth = maxDepth(T->lchild)+1;
+    int rDepth = maxDepth(T->rchild)+1;
+    return lDepth > rDepth ? lDepth : rDepth;
+}
+
+/*Leetcode 111
+找到二叉树的最小深度  需要找到叶节点  才算遍历了完整的路径
+需要注意  一定要检索到了叶子节点才算  即节点的左右孩子均为NULL
+*/
+int minDepth(BiTree T)
+{
+    if(T==NULL)
+        return 0;
+    if(T->lchild==NULL)
+        return (minDepth(T->rchild)+1);
+    if(T->rchild==NULL)
+        return (minDepth(T->lchild)+1);
+    return (Min(minDepth(T->lchild),minDepth(T->rchild))+1);
+}
+/*Leetcode 112
+给定一棵树和一个整数  决定是否该数有从根节点到叶节点的一条路径上的和等于给定的这个整数
+思路： 判断是否为叶节点  不是叶节点继续向下叠加求节点的和  是叶节点则判断是否等于给定的整数
+*/
+int isHasPathSum(BiTree T,int sum)
+{
+    if(T==NULL)
+        return FALSE;
+    if(T->lchild==NULL && T->rchild==NULL && sum==T->data)  //左右节点都为NULL  叶节点
+        return TRUE;
+    return (isHasPathSum(T->lchild,sum-(T->data))||isHasPathSum(T->rchild,sum-(T->data)));
+}
+
 
 
 
--- binary_tree/bin_tree.h
+++ binary_tree/bin_tree.h
@@ -6,10 +6,21 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <math.h>
 
 void func(void);
+enum {
+    OK = 0,
+    Error1 = 1,
+    Error2 = 2,
+};
+enum {
+    FALSE = 0,
+    TRUE = 1
+};
 
 
+typedef char Status;
 typedef unsigned char    TElemType;
 typedef struct BiTNode
 {
@@ -17,9 +28,15 @@ typedef struct BiTNode
     struct BiTNode *lchild,*rchild;
 }BiTNode,*BiTree;
 
+#define Max(x,y) (x>y?x:y)
+#define Min(x,y) (x>y?y:x)
 
 void CreatBiTree(BiTree *T);
 void PreOrderTraverse(BiTree T);
 void InOrderTraverse(BiTree T);
+
+int maxDepth(BiTree T);
+int minDepth(BiTree T);
+int isHasPathSum(BiTree T,int sum);
 
 #endif
